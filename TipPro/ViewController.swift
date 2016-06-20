24e626f8342bc3a01673da88bde85014de3be045
Tsz Chun Hui <jeffreyhui04@hotmail.com>
2016-06-20 13:45:19 -0700

    Initial Commit

---
+++ TipPro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipPro
+//
+//  Created by Jeff Hui on 20/6/2016.
+//  Copyright © 2016 AffixGrp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
